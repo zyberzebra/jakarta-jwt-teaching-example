e70541e515e1c7108efccbef24d8da5c5a05bf00
ZybrZebra <zyberzebra@users.noreply.github.com>
2024-03-18 21:46:11 +0100

    init

---
+++ src/main/java/com/example/demo1/CookieVaultServlet.java
@@ -0,0 +1,41 @@
+package com.example.demo1;
+
+import java.io.*;
+import java.time.LocalDate;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "cookieVaultServlet", value = "/the-cookie-vault")
+public class CookieVaultServlet extends HttpServlet {
+    public static final LocalDate CHEAP_SALT = LocalDate.now();
+    public static final Algorithm ALGORITHM = Algorithm.HMAC256("tomcat"+ CHEAP_SALT);
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        String jwt = JWT.create().withClaim("Secret", "Hello Hacker. This is your price.").sign(ALGORITHM);
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<div>"+ "you fond your JWT! "
+                +jwt
+                +" I Stored it in your cookies."
+                +"<div>");
+        out.println("</body></html>");
+        Cookie cookie = new Cookie("JWT", jwt); //TODO Timeout needed
+        response.addCookie(cookie);
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Hello World!" %>
+</h1>
+<br/>
+<a href="the-cookie-vault">The Cool Cookie Vault</a>
+<a href="secret">Get your cookie first!</a>
+</body>
+</html>
